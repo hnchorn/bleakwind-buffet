39b2c8d9134ec6138fccd82e4e4c92bae8a0b3cc
hnchorn <16204@LAPTOP-CPC268I0>
2020-11-08 22:49:28 -0600

    Finished commenting and updating UML.

--- Website/Pages/Index.cshtml.cs
+++ Website/Pages/Index.cshtml.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*
+ * Author: Hannah Chorn
+ * Class: Index.cshtml.cs
+ * Purpose: Represents the home page of the Bleakwind Buffet website and what is displayed on it.
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,28 +20,43 @@ namespace Website.Pages
 {
     public class IndexModel : PageModel
     {
-        /*
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
-        */
-
+        /// <summary>
+        /// List of items to be displayed on the menu.
+        /// </summary>
         public IEnumerable<IOrderItem> Items { get; set; }
 
+        /// <summary>
+        /// Search term(s) entered into search bar.
+        /// </summary>
         public string SearchTerms { get; set; }
 
+        /// <summary>
+        /// Filtering category of item type. (Entree, side, or drink)
+        /// </summary>
         public IEnumerable<string> Types { get; set; }
 
+        /// <summary>
+        /// List of entrees to be displayed on the menu.
+        /// </summary>
         public List<Entree> Entrees { get; set; }
 
+        /// <summary>
+        /// List of sides to be displayed on the menu.
+        /// </summary>
         public List<Side> Sides { get; set; }
 
+        /// <summary>
+        /// List of drinks to be displayed on the menu.
+        /// </summary>
         public List<Drink> Drinks { get; set; }
 
-
+        /// <summary>
+        /// Updates the menu depending on search and filtering options.
+        /// </summary>
+        /// <param name="CaloriesMin">Minimum number of calories to filter.</param>
+        /// <param name="CaloriesMax">Maximum number of calories to filter.</param>
+        /// <param name="PriceMin">Minimum price to filter.</param>
+        /// <param name="PriceMax">Maximum price to filter.</param>
         public void OnGet(int? CaloriesMin, int? CaloriesMax, double? PriceMin, double? PriceMax)
         {
             Items = Menu.FullMenu();
